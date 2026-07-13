0d98833373338de25dbe623ea08d21031552fa96
Rain Wu <42561302+RainWuAme@users.noreply.github.com>
2026-07-13 21:24:10 +0800

    Add: word-ladder - submission-6

---
+++ Data Structures & Algorithms/word-ladder/submission-6.py
@@ -0,0 +1,33 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+
+        nei = defaultdict(list)
+        wordList.append(beginWord)
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j+1:]
+                nei[pattern].append(word)
+
+        # BFS shortest distance in graph
+        q = deque()
+        q.append(beginWord)
+        visit = set([beginWord])
+        res = 1
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j+1:]
+                    for nei_w in nei[pattern]:
+                        if nei_w not in visit:
+                            visit.add(nei_w)
+                            q.append(nei_w)
+            res += 1
+
+        return 0
+
+
